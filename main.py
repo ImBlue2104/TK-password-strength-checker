16aaf85a2310ff0728939a9bf82a4660fb820d4e
Neel Patil <neel.rp21@gmail.com>
2025-09-16 23:37:42 -0700

    git checkout feature\g

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from tkinter import Button, Label, Entry, Toplevel
+from tkinter import Button, Label, Entry, Toplevel, Tk
+
+#Create Window
+root = Tk()
+root.title("Password Strength Checker App")
+root.geometry("400x600")
+
+welcome_msg = Label(text = "Welcome to the Password Strength Checker App", fg="white", bg="black").pack()
+description_msg = Label(text="Enter your password to check its strength based on length!",fg="white", bg="black").pack()
+password_input = Entry().pack()
+
